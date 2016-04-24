ab6432d7240d78e553ae34421e0f685eb2d799fa
Matt Sloan <msloan@knotice.com>
2016-04-24 12:57:29 -0400

    initial file structure set up and commented js

--- multiple-accordions-tabs.js
+++ multiple-accordions-tabs.js
@@ -1,66 +1,81 @@
- $(document).ready(function () {
+$(document).ready(function () {
 	
 	// MULTIPLE ACCORDIONS AND TABS (WITH QUERY STRING AND SCROLL OPTIONS)
+
+  //Begin Accordion Functionality
+
+  // hide all accordion content 
+  // (the next element after the accordion header)
 	$('.expand')
 		.next()
 		.hide();
 
+  // Use "view" querystring parameter to determine
+  // which accordion item to show on load
 	var item = get_urid('view');
 	item = parseInt(item);
 	if(item) {
 		openAccordion($('#' + item));
 	}
+  // End Accordion Functionality
 
 	// MULTIPLE TABS
+
+  // Begin Tabbed System Functionality
+  // Add ID to each set of tabs
 	$('.tab-system').each(function(n) {
 		$(this).attr('id', "tab-system0" + (n + 1));
 	});
 
+  // get "system" querystring parameter to determine which
+  // tabbed system we will be focusing on
 	var system = get_urid('system');
+
+  
 	if(item) {
-    	if(system) {
-         	var selector = '#tab-system0' + system;
-            scrollToDiv($(selector));
-        } else {
-            var selector = '.tab-system';
-        }
-
-        $('.tab').each(function() {
-           	if($(this).index() != 0) {
-            	$(this).hide();
-           	}
-        });
-
-        $(selector + ' .tab').each(function() {
-           	if($(this).index() == item - 1) {
-                $(this).show();
-           	} else {
-            	$(this).hide();
-           	}
-        });
-
-        $('.tabs').each(function() {
-            $('.tabs-nav li:first', this).addClass('current');
-            $('.tabs-content-container .tab:first-child', this).show();
-        });
-
-        $(selector + ' .tabs').each(function() {
-            item = item.toString();
-            $('.tabs-nav li', this).removeClass('current');
-            $('.tabs-nav li:nth-child(' + item + ')', this).addClass('current');
-        });
+  	if(system) {
+      var selector = '#tab-system0' + system;
+      scrollToDiv($(selector));
     } else {
-        $('.tab').each(function() {
-           	if($(this).index() != 0) {
-            	$(this).hide();
-           	}
-        });
-
-        $('.tabs').each(function() {
-            $('.tabs-nav li:first', this).addClass('current');
-            $('.tabs-content-container .tab:first-child', this).show();
-        });
+      var selector = '.tab-system';
     }
+
+    $('.tab').each(function() {
+     	if($(this).index() != 0) {
+      	$(this).hide();
+     	}
+    });
+
+    $(selector + ' .tab').each(function() {
+     	if($(this).index() == item - 1) {
+        $(this).show();
+     	} else {
+      	$(this).hide();
+     	}
+    });
+
+    $('.tabs').each(function() {
+      $('.tabs-nav li:first', this).addClass('current');
+      $('.tabs-content-container .tab:first-child', this).show();
+    });
+
+    $(selector + ' .tabs').each(function() {
+      item = item.toString();
+      $('.tabs-nav li', this).removeClass('current');
+      $('.tabs-nav li:nth-child(' + item + ')', this).addClass('current');
+    });
+  } else {
+    $('.tab').each(function() {
+     	if($(this).index() != 0) {
+      	$(this).hide();
+     	}
+    });
+
+    $('.tabs').each(function() {
+      $('.tabs-nav li:first', this).addClass('current');
+      $('.tabs-content-container .tab:first-child', this).show();
+    });
+  }
 
 	$('.tabs-nav li').click(function (e) {
 		var tabSection = $(this).closest('article').attr('id');
@@ -72,9 +87,10 @@
 		$('#' + tabSection + ' .tabs-content div.tab:nth-child(' + tabNum + ')').fadeIn('fast');
 		e.preventDefault();
 	});
-	// END MULTIPLE ACCORDIONS AND TABS IN DOC READY
+  // End Tabbed System Functionality
+  // END MULTIPLE ACCORDIONS AND TABS IN DOC READY
 	
- });	
+});	
 
 
 // ACCORDION AND TAB FUNCTIONS
