44cf7610213e11478703ad230195e78a8ddeaed0
Matt Sloan <msloan@knotice.com>
2016-04-24 13:41:40 -0400

    commented each line/block of tab js, minor updates to styling, added placeholder page content to html to ensure scrollToDiv is working.

--- multiple-accordions-tabs.js
+++ multiple-accordions-tabs.js
@@ -26,27 +26,28 @@ $(document).ready(function () {
   // Begin Tabbed System Functionality
   // Add ID to each set of tabs
 	$('.tab-system').each(function(n) {
-		$(this).attr('id', "tab-system0" + (n + 1));
+		$(this).attr('id', "tab-system" + (n + 1));
 	});
 
-  // get "system" querystring parameter to determine which
-  // tabbed system we will be focusing on
-
-
 	if(view) {
+
   	if(system) {
-      var selector = '#tab-system0' + system;
+      //if "system" querystring is set, go to that div using scrollToDiv
+      var selector = '#tab-system' + system;
       scrollToDiv($(selector));
     } else {
+      //if no "system" querystring, assume 1st tab system, and no scroll on-load
       var selector = '.tab-system';
     }
 
+    //show 1st tab by default, hide all other tabs
     $('.tab').each(function() {
      	if($(this).index() != 0) {
       	$(this).hide();
      	}
     });
 
+    //set tab of proper tabbed system to show the tab specified in the view parameter, hide other tabs
     $(selector + ' .tab').each(function() {
      	if($(this).index() == view - 1) {
         $(this).show();
@@ -55,56 +56,76 @@ $(document).ready(function () {
      	}
     });
 
+    //add "current" class to first tab in each system 
     $('.tabs').each(function() {
       $('.tabs-nav li:first', this).addClass('current');
       $('.tabs-content-container .tab:first-child', this).show();
     });
 
+    //add "current" class to specified tab based on what was put in the "view" querystring parameter
     $(selector + ' .tabs').each(function() {
       view = view.toString();
       $('.tabs-nav li', this).removeClass('current');
       $('.tabs-nav li:nth-child(' + view + ')', this).addClass('current');
     });
+
+  // if no "view" querystring parameter
   } else {
+
+    //show 1st tab by default, hide all other tabs
     $('.tab').each(function() {
      	if($(this).index() != 0) {
       	$(this).hide();
      	}
     });
 
+    //add "current" class to first tab in each system 
     $('.tabs').each(function() {
       $('.tabs-nav li:first', this).addClass('current');
       $('.tabs-content-container .tab:first-child', this).show();
     });
   }
 
-	$('.tabs-nav li').click(function (e) {
+  //Tab navigation functionality
+	$('.tabs-nav li').click(function () {
+
+    //find which article we are in and get its ID
 		var tabSection = $(this).closest('article').attr('id');
+
+    //hide all tabs for that tab system (that you clicked within)
 		$('#' + tabSection + ' .tabs-content div.tab').hide();
+
+    //remove "current" class from associated tabbed system
 		$('#' + tabSection + ' .tabs-nav .current').removeClass('current');
+
+    //add "current" class to the tab clicked
 		$(this).addClass('current');
 
+    //find tab number clicked
 		var tabNum = $(this).index() + 1;
+
+    //fade in content of tab number clicked
 		$('#' + tabSection + ' .tabs-content div.tab:nth-child(' + tabNum + ')').fadeIn('fast');
-		e.preventDefault();
+
+		return false;
 	});
   // End Tabbed System Functionality
   // END MULTIPLE ACCORDIONS AND TABS IN DOC READY
 	
-});	
+});	// End Doc Ready
 
 
 // ACCORDION AND TAB FUNCTIONS
-$('.expand').on('click', function () {
-	var thisID = $(this).attr('id');
+// $('.expand').on('click', function () {
+// 	var thisID = $(this).attr('id');
 
-	if ($(this).hasClass('expanded')) {
-		closeAccordion($(this));
-	} else {
-		openAccordion($(this));
-	}
-	return false;
-});
+// 	if ($(this).hasClass('expanded')) {
+// 		closeAccordion($(this));
+// 	} else {
+// 		openAccordion($(this));
+// 	}
+// 	return false;
+// });
 
 // function closeAccordion(view) {
 // 	$(view)
